eef20e23f3864ec41d1606900d37389da14e2fc4
user326 <a01708940@tec.mx>
2023-11-23 03:04:00 -0700

    fix: NetAPIService structure

--- MobileExam/NetworkAPIService.swift
+++ MobileExam/NetworkAPIService.swift
@@ -10,22 +10,22 @@ import Alamofire
 
 class NetworkAPIService {
     static let shared = NetworkAPIService()
-}
-
-func getMovies(url: URL) async -> Movies?{
-    let taskRequest = AF.request(url, method: .get).validate()
-    let response = await taskRequest.serializingData().response
     
-    switch response.result {
-        case .success(let data):
-            do{
-                return try JSONDecoder().decode(Movies.self, from: data)
-            }
-            catch{
+    func getMovies(url: URL) async -> Movies?{
+        let taskRequest = AF.request(url, method: .get).validate()
+        let response = await taskRequest.serializingData().response
+        
+        switch response.result {
+            case .success(let data):
+                do{
+                    return try JSONDecoder().decode(Movies.self, from: data)
+                }
+                catch{
+                    return nil
+                }
+            case let .failure(error):
+                debugPrint(error.localizedDescription)
                 return nil
-            }
-        case let .failure(error):
-            debugPrint(error.localizedDescription)
-            return nil
+        }
     }
 }
