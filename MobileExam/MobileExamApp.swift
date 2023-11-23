7273d048244af0a796296d38085cac606773feee
user326 <a01708940@tec.mx>
2023-11-22 23:59:24 -0700

    Initial Commit

---
+++ MobileExam/MobileExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileExamApp.swift
+//  MobileExam
+//
+//  Created by user326 on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MobileExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
